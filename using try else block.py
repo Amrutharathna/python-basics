20d6894900c55bede3cacdda3cdf447906127981
Amrutharathna <167096548+Amrutharathna@users.noreply.github.com>
2024-04-18 10:05:00 +0530

    Create using try else block.py

---
+++ using try else block.py
@@ -0,0 +1,37 @@
+try:
+    a=10/0
+except:
+    print("some exceptions raised")
+else:
+    print("no exceptions raised,everything worked perfectly")
+finally:
+    print("this ia final block")
+print("outside try block")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
